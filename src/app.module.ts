3e26bb20f2a2c96dbc06305695817531ed01e3d2
xuanyu <969718197@qq.com>
2021-09-18 17:48:15 +0800

    :zap:添加动态的邮件Html模板

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2020-10-20 10:11:57
- * @LastEditTime: 2021-09-07 15:09:24
+ * @LastEditTime: 2021-09-18 16:11:20
  * @Description: Modify here please
  */
 import { Module } from '@nestjs/common';
--- src/send-email/send-email.service.ts
+++ src/send-email/send-email.service.ts
@@ -4,7 +4,7 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2021-03-29 11:40:44
- * @LastEditTime: 2021-08-10 16:03:00
+ * @LastEditTime: 2021-09-18 17:28:18
  * @Description: Modify here please
  */
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
@@ -12,18 +12,37 @@ import { MailerService } from '@nestjs-modules/mailer';
 import { mongoose, ReturnModelType } from '@typegoose/typegoose';
 import { InjectModel } from 'nestjs-typegoose';
 import { Member } from '@libs/db/models/member.model';
+import * as Handlebars  from 'handlebars';
+import { readFileSync } from 'fs';
+import path = require('path');
 @Injectable()
 export class SendEmailService {
   constructor(private readonly emailService: MailerService,@InjectModel(Member)
   private readonly memberModel: ReturnModelType<typeof Member>) { };
 
-  sendEmail(): void {
+  // 读取邮件模板
+  /**
+   * @description: 
+   * @param {string} templateName 模板名称，提示：以template目录下，文件名为名称！
+   * @param {object} data 传递模板数据 注意：类型为对象
+   * @return { string } 返回 邮件模板内容
+   */
+   renderEmailHtml(templateName:string, data = {}): string {
+    const readHtml = readFileSync(path.join(__dirname, `./send-email/template/${templateName}.html`), 'utf8');
+    const renderHtml = Handlebars.compile(readHtml)
+    const vHtml =  renderHtml(data)
+    return vHtml;
+  }
+  
+   sendEmail(): void {
+    const data = { name: 'xauanyu54564', list: [ { title: '标题1', content: '内容一' }, { title: '标题2', content: '内容2' } ] }
+    const html = this.renderEmailHtml('test', data)
     this.emailService.sendMail({
       to: '969718197@qq.com', // 接收信息的邮箱
       from: '812006298@qq.com', // 要发送邮件的邮箱
       subject: 'Love You √',
-      // html: '<b>welcome !</b><br/><p>测试邮件发送成功!</p>',
-      template: 'email',
+      html: html,
+      // template: 'email',
     }).then(() => {
       console.log("邮件发送成功！")
     }).catch(() => {
